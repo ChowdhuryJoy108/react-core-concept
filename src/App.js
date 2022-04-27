63e8d8918929c361fe7ef344d7c7d8e9bd3e374c
Joy Chowdhury <imjoychy69@gmail.com>
2022-04-27 20:07:51 +0600

    End

--- src/App.js
+++ src/App.js
@@ -3,7 +3,24 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [nayoks3 , SetNayoks] = useState([]);
+
+  useEffect(()=>{
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then(res =>res.json())
+    .then(data =>SetNayoks(data));
+  }, [])
+
+
+
   const nayoks = ['Anwar', 'jafor', 'alomgir', 'salman','Bappi','shuvo','Sakib k'];
+
+  const nayoks2 =[
+    {name:'jasim', age: 56},
+    {name:'sakib k', age: 39},
+    {name:'deepjol', age:55}
+  ];
 
   const products = [
     {name:'photoshop', price:'$90.99', id:102},
@@ -18,6 +35,29 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>I m  a react person</p>
+
+        {
+          nayoks3.map(nayok => <Nayok3 name={nayok.name} key={nayok.id} email={nayok.email} ></Nayok3>)
+        }
+
+        {
+          nayoks2.map(nayok => <Nayok2 name={nayok.name} age={nayok.age}></Nayok2>)
+        }
+
+        
+        
+        <MovieCounter />
+
+      {
+        nayoks.map(nayok => <Nayok name={nayok} />)
+      }
+
+
+
+        {/* <Nayok name={nayoks2[0]} age='56 '/>
+        <Nayok name={nayoks2[1]}  />
+        <Nayok name='Bappi'/>
+        <Nayok name='Bappa R' /> */}
         <Shiva name="Shiva" say="Om Namay Shiva" />
 
         <Counter />
@@ -112,12 +152,58 @@ function Shiva(props){
 
 
 
+function MovieCounter(){
+
+  const [count, setCount] = useState(0);
+  return(
+    <div>
+      <button onClick={()=>setCount(count + 1)}>add movie</button>
+      <h5>Number of movies : {count} </h5>
+      <MovieDisplay movies={count}/>
+      <MovieDisplay movies={count + 2} />
+      <MovieDisplay movies={count + 5}/>
+      <MovieDisplay movies={count + 10}/>
+    </div>
+  )
+}
+
+function MovieDisplay(props){
+
+  return <h4> Movies I hacve acted : {props.movies} </h4>
+}
 
 
 
 
+function Nayok (props){
+ 
+  
+  return(
+    <div style={{border:'1px solid red', width:'200px',margin:'10px'}}>
+      <h2>Ami Nayok- {props.name}</h2>
+      <p>I have done 5 movies in {props.age || 30} years</p>
+    </div>
+  )
+}
 
 
+function Nayok2 (props){
+  return (
+    <div style={{border:'2px solid yellow', width:'200px', height:'200px', margin:'10px', padding:'10px'}}>
+      <h2>Ami Nayok - {props.name}</h2>
+      <p>I have complete {props.age} years in industry!</p>
+    </div>
+  )
+}
+
+function Nayok3(props){
+  return(
+    <div style={{border:'2px solid yellow', width:'280px', height:'280px', margin:'10px', padding:'10px', fontSize:"16px"}}>
+      <h1>Nayok - {props.name} </h1>
+      <p>Contract - {props.email} </p>
+    </div>
+  )
+}
 
 
 
